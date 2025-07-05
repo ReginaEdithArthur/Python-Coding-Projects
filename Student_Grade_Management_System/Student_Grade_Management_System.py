ff9137b30370aa615f8a0dd87a627c61ad108bd5
ReginaEdithArthur <reginaeditharthur@gmail.com>
2025-07-05 20:14:00 +0000

    Step in Project

--- Student_Grade_Management_System/Student_Grade_Management_System.py
+++ Student_Grade_Management_System/Student_Grade_Management_System.py
@@ -1,1 +1,15 @@
-'''This is the folder I will be working in'''
+#Greer users and confirm intent to use the system
+
+#Initize an empty list to store students data
+
+#Start data entry loop
+
+#Calculate grade for the score
+
+#Store student record in the list
+
+#Data entry and calculation of class average
+
+#Display table of student data
+ 
+#Display class average
