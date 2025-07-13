3a8f81f05016cc79cb14c15215e19a6d1de8ec7c
ReginaEdithArthur <reginaeditharthur@gmail.com>
2025-07-13 22:48:21 +0000

    Clean code improvement

--- Student_Grade_Management_System/Student_Grade_Management_System.py
+++ Student_Grade_Management_System/Student_Grade_Management_System.py
@@ -2,16 +2,26 @@
 print ('Welcome, you can enter student grades for your course.')
 go_ahead = input ('Would you like to enter student grades? ')
 
+def student_details(student_info, name, score):
+    return 
+
+#Initize an empty dictionary to store students name and score
+student_info= {}
+
 #confirm intent to use the system
-if go_ahead.upper() == "Yes":
-    start = 1
-else :
-    start = 0
-while start = 0:
+if go_ahead.upper() != "Yes":
+    while True:
+        #Enter student name
+        student_name= input("Please enter a single student's name: ")
+        #student_name= student_details
+        student_score= input("Please enter that student's score: ")
 
 
+
+    #If statement to check if check if user is done entering data
     
-#Initize an empty list to store students data
+#Initize an empty list to store students name and score
+
 
 #Start data entry loop
 
