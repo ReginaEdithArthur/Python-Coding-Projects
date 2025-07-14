60efdaa6d8c778ed3720f021b2065956b930955d
ReginaEdithArthur <reginaeditharthur@gmail.com>
2025-07-14 20:56:53 +0000

    It is running

--- Student_Grade_Management_System/Student_Grade_Management_System.py
+++ Student_Grade_Management_System/Student_Grade_Management_System.py
@@ -3,18 +3,29 @@ print ('Welcome, you can enter student grades for your course.')
 go_ahead = input ('Would you like to enter student grades? ')
 
 def student_details(student_info, name, score):
-    return 
+    student_info[name] = score
+    list = student_info
+    return list
 
 #Initize an empty dictionary to store students name and score
 student_info= {}
 
 #confirm intent to use the system
-if go_ahead.upper() != "Yes":
+if go_ahead.upper() == "Yes":
     while True:
         #Enter student name
         student_name= input("Please enter a single student's name: ")
         #student_name= student_details
         student_score= input("Please enter that student's score: ")
+        #Pass to student detail function
+        details = student_details(student_info, student_name, student_score)
+        
+        #Check if they want to continue
+        go_ahead = input("Would you like to continue: ")
+        #confirm intent to continue using the system
+        if go_ahead.upper() != "Yes":
+            break
+
 
 
 
