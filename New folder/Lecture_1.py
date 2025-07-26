706d8e6f2d8ee2baabd6310919e56ea76a8f82e2
ReginaEdithArthur <reginaeditharthur@gmail.com>
2025-07-26 21:03:49 +0000

    Lecture 1 Practice

---
+++ New folder/Lecture_1.py
@@ -0,0 +1,19 @@
+name='Ana'
+print(name[1])
+print(len(name))
+print((13-4)/(12*12))
+
+#Promble set 0 
+#Name: Regina Arthur
+#Collaborator: Me
+#Time Spent: 1:00
+
+import math
+print(dir(math))
+
+x = 5
+y = 8
+z = x + y
+
+a = math.log2(z)
+print(a)
